84693191daec52e545c9800e9d476cf05b0b8d96
Gennady <111905354+GennadyBerg@users.noreply.github.com>
2024-03-21 19:54:54 +0200

    entities universal with permisiions w/o upload_1

--- routes/postRouter.js
+++ routes/postRouter.js
@@ -1,14 +1,14 @@
 const { Router } = require('express');
 const { passport } = require('../middleware/passport-middleware.js');
 const { getAllPosts, getPostById, createNewPost, deletePostById, putPostById, patchPostById } = require('../controllers/postController.js');
-const { userAuth, extractEntity } = require('../middleware/authorization-middleware.js');
+const { userAuth, extractEntity, adminAuth } = require('../middleware/authorization-middleware.js');
 const multer = require('multer');
 const os = require('os');
 
 const postRouter = new Router();
 const upload = multer({ dest: os.tmpdir() });
 
-postRouter.post('/posts', passport.authenticate('sign-in-token', { session: false }), upload.single('file'), extractEntity, userAuth, createNewPost);
+postRouter.post('/posts', passport.authenticate('sign-in-token', { session: false }), upload.single('file'), extractEntity, userAuth, adminAuth, createNewPost);
 postRouter.put('/posts/:id', passport.authenticate('sign-in-token', { session: false }), extractEntity, userAuth, putPostById);
 postRouter.patch('/posts/:id', passport.authenticate('sign-in-token', { session: false }), extractEntity, userAuth, patchPostById);
 postRouter.delete ('/posts/:id', passport.authenticate('sign-in-token', { session: false }), extractEntity, userAuth, deletePostById);
